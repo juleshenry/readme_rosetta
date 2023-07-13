c3aa2006d047b387a8d58eeec4fd62e34a1b344b
Julian Henry <julianplushenry@gmail.com>
2023-07-12 22:49:18 -0500

    /Users/mariopokemon/former/projs/git-goblin/gacp

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Consumes README.md's, yields folders and hyperlinks to translated README.md's
 
 ![alt text](https://upload.wikimedia.org/wikipedia/commons/7/72/The_Tower_of_Babel.jpg)
 
-CLI: `readme-babel kor esp zh --out READMEs` -> 
+CLI: `readme-babel kor esp zh README.md --out READMEs` -> 
 
 READMEs\
 	--README.md.kor
@@ -12,3 +12,10 @@ READMEs\
 
 default --out : "README-translations"
 
+Update: 
+
+Wed Jul 12 22:42:16 CDT 2023
+
+Argostranslate is broken. I cannot verify SSL required for its index.json data.
+
+I may tact a new tact or library to accomplish this initiative
--- babel.py
+++ babel.py
@@ -2,13 +2,15 @@ import argostranslate.package
 import argostranslate.translate
 import sys
 
+
 def trans(from_code, to_code, text):
     # Download and install Argos Translate package
-    argostranslate.package.update_package_index()    
+    argostranslate.package.update_package_index()
     available_packages = argostranslate.package.get_available_packages()
     available_package = list(
         filter(
-            lambda x: x.from_code == from_code and x.to_code == to_code, available_packages
+            lambda x: x.from_code == from_code and x.to_code == to_code,
+            available_packages,
         )
     )[0]
     download_path = available_package.download()
@@ -16,15 +18,12 @@ def trans(from_code, to_code, text):
 
     # Translate
     installed_languages = argostranslate.translate.get_installed_languages()
-    from_lang = list(filter(
-            lambda x: x.code == from_code,
-            installed_languages))[0]
-    to_lang = list(filter(
-            lambda x: x.code == to_code,
-            installed_languages))[0]
+    from_lang = list(filter(lambda x: x.code == from_code, installed_languages))[0]
+    to_lang = list(filter(lambda x: x.code == to_code, installed_languages))[0]
     translation = from_lang.get_translation(to_lang)
     translatedText = translation.translate(text)
     return translatedText
+
 
 if __name__ == "__main__":
     sa = sys.argv
